2bc0cb09f3abf59a2e73e802092e5708a433a245
dogyun <a2256014@naver.com>
2022-12-29 13:27:57 +0900

    error fix

--- BackendServer/lib/api/Beneficiary/index.js
+++ BackendServer/lib/api/Beneficiary/index.js
@@ -5,21 +5,12 @@ var add = require("./add");
 var approve = require("./approve");
 var pending = require("./pending");
 var view = require("./view");
-<<<<<<< HEAD
-var remove = require("./delete");   
-=======
 var delete2 = require("./delete");
-
->>>>>>> 2ed1637c7f4c416475e0fb026fa081f0a2e8409c
 
 router.use("/add", add);
 router.use("/approve", approve);
 router.use("/pending", pending);
 router.use("/view", view); // Give options on FE to see pending list as well
-<<<<<<< HEAD
-router.use("/delete", remove);
-=======
 router.use("/delete", delete2);
->>>>>>> 2ed1637c7f4c416475e0fb026fa081f0a2e8409c
 
 module.exports = router;
